8cb977a581cdc27dd38ba3efd3d1c30c6d3d2ec6
lif314 <2216155077@qq.com>
2022-09-30 17:02:35 +0800

    stereo vision

---
+++ ch5-Camera_and_Images/3-stereoVision/stereoVision.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file stereoVision.cpp
+ * @author your name (you@domain.com)
+ * @brief 3D视觉
+ * @version 0.1
+ * @date 2022-09-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+/**
+ * @brief 双目视觉成像原理
+ * 
+ * 从双目视觉左右图像出发，计算图像对应的视差图，然后计算各像素在坐标系下的坐标，他们将构成点云
+ * 
+ */
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+
